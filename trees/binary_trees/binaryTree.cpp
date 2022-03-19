4d1ad6c1d8ed293f22e7235ee797188f00ae061f
Akshat Samdani <akshatsamdani23@gmail.com>
2022-03-19 15:59:23 +0530

    code for creation of binary tree

--- trees/binary_trees/binaryTree.cpp
+++ trees/binary_trees/binaryTree.cpp
@@ -14,7 +14,35 @@ class Node{
     }
 };
 
+class binaryTree{
+    Node* root;
+    public:
+    binaryTree(){
+        root = NULL;
+    }
+
+    Node* buildTree(){
+        cout << "Enter the data: " << endl;
+        int data;
+        cin >> data;
+        
+        root = new Node(data);
+
+        if(data == -1){
+            return NULL;
+        }
+
+        cout << "Enter  data for left of " << data << ": " << endl;
+        root->left = buildTree();
+        cout << "Enter  data for right of " << data << ": " << endl;
+        root->right = buildTree();
+        return root;
+
+    }
+};
 int main(){
+    binaryTree bt;
+    bt.buildTree();
 
     return 0;
 }
