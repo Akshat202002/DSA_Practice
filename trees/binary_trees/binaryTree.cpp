beff3de5c226705e1643823da68990c313910e0a
Akshat Samdani <akshatsamdani23@gmail.com>
2022-03-19 16:50:45 +0530

    updated code for creation of binary tree

--- trees/binary_trees/binaryTree.cpp
+++ trees/binary_trees/binaryTree.cpp
@@ -14,35 +14,28 @@ class Node{
     }
 };
 
-class binaryTree{
-    Node* root;
-    public:
-    binaryTree(){
-        root = NULL;
+Node* buildTree(Node* root){
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    
+    root = new Node(data);
+
+    if(data == -1){
+        return NULL;
     }
 
-    Node* buildTree(){
-        cout << "Enter the data: " << endl;
-        int data;
-        cin >> data;
-        
-        root = new Node(data);
+    cout << "Enter  data for left of " << data << ": " << endl;
+    root->left = buildTree(root-> left);
+    cout << "Enter  data for right of " << data << ": " << endl;
+    root->right = buildTree(root-> right);
+    return root;
 
-        if(data == -1){
-            return NULL;
-        }
+}
 
-        cout << "Enter  data for left of " << data << ": " << endl;
-        root->left = buildTree();
-        cout << "Enter  data for right of " << data << ": " << endl;
-        root->right = buildTree();
-        return root;
-
-    }
-};
 int main(){
-    binaryTree bt;
-    bt.buildTree();
+    Node* root = NULL;
+    buildTree(tree);
 
     return 0;
 }
