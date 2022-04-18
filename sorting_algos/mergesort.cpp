0b68482384006dd08f392c7bab42c7c2b4afb086
Akshat Samdani <akshatsamdani23@gmail.com>
2022-04-18 16:16:54 +0530

    added comments

--- sorting_algos/mergesort.cpp
+++ sorting_algos/mergesort.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 using namespace std;
+
+// swap two elements
 void swap(int *x, int *y)
 {
     int temp = *x;
     *x = *y;
     *y = temp;
 }
+
+// merge function which merges two sorted arrays
 void Merge(int A[], int l, int mid, int h)
 {
+    // variable to store the size of left(i) and right subarrays(j) and
+    // k is the index of the resultant array
     int i = l, j = mid + 1, k = l;
+    // create a temp array
     int B[100];
+
+    // copy data to temp array
     while (i <= mid && j <= h)
     {
         if (A[i] < A[j])
@@ -17,21 +26,31 @@ void Merge(int A[], int l, int mid, int h)
         else
             B[k++] = A[j++];
     }
+
+    // copy remaining elements from left array
     for (; i <= mid; i++)
         B[k++] = A[i];
+    // copy remaining elements from right array
     for (; j <= h; j++)
         B[k++] = A[j];
+    // copy back to original array
     for (i = l; i <= h; i++)
         A[i] = B[i];
 }
+
+// merge sort function which sorts the array
 void MergeSort(int A[], int l, int h)
 {
     int mid;
     if (l < h)
     {
+        // find the mid index
         mid = (l + h) / 2;
+        // sort the left subarray
         MergeSort(A, l, mid);
+        // sort the right subarray
         MergeSort(A, mid + 1, h);
+        // merge the two subarrays
         Merge(A, l, mid, h);
     }
 }
